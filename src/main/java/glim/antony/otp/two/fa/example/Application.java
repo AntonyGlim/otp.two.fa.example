fc52f4a5ba24309094dc6f64b85f34bbcbfe578d
Antony <a.yatsenko@aeronavigator.ru>
2021-10-18 18:54:52 +0300

    Initial commit

---
+++ src/main/java/glim/antony/otp/two/fa/example/Application.java
@@ -0,0 +1,13 @@
+package glim.antony.otp.two.fa.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/glim/antony/otp/two/fa/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package glim.antony.otp.two.fa.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
